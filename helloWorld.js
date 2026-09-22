4e3129d8940626729d5b5890cdc91e190763c427
Chabloz Nicolas (SEC-GC) <CHABLOZN@ge-admin.ad.etat-ge.ch>
2026-09-22 09:33:42 +0200

    first commit

---
+++ helloWorld.js
@@ -0,0 +1,32 @@
+const x = 0.1 + 0.2;
+console.log(x);
+
+if (true) {
+
+} else {
+
+}
+
+for (let i=0; i<100; i++) {
+
+}
+
+function doubleVal(val) {
+  return val * 2;
+}
+
+doubleVal(x)
+
+function foo() {
+  return 'test';
+}
+
+const truc = function () {
+  console.log("i'm a function");
+}
+
+function make(thing){
+  thing();
+}
+
+make(truc);
